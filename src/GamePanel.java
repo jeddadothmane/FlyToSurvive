d7a2952ffe9eca739b14479d4e0bb883c163cab7
jeddadothmane <othmane.jeddad20@gmail.com>
2023-06-11 00:41:22 +0200

    Panel first implementation

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -8,6 +8,8 @@ import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel implements KeyListener, ActionListener {
     final int HEIGHT = 550, WIDTH = 525;
+    int shipHeight = HEIGHT/2;
+    int shipV=0, shipA=0, shipI=0;
     public GamePanel(){
 
 
@@ -15,21 +17,41 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
         setFocusable(true);
         addKeyListener(this);
         setBackground(Color.BLACK);
-    }
-    public void actionPerformed(ActionEvent e){
-        repaint();
+        new Timer(40,this).start();
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
+        drawShip(g);
 
     }
+    private void drawShip(Graphics g){
+        g.setColor(Color.WHITE);
+        g.fillRect(75 , 400 + shipV, 50,50);
+
+    }
+    public void actionPerformed(ActionEvent e){
+        shipA += shipI;
+        shipV += shipA;
+        repaint();
+    }
+
     public void keyTyped(KeyEvent e){
 
     }
 
 
     public void keyPressed(KeyEvent e){
+        int code = e.getKeyCode();
+        if(code == e.VK_UP){
+            shipA=-8;
+            shipI=0;
+        }
+        if(code == e.VK_DOWN){
+            shipA=+8;
+            shipI=0;
+        }
+
 
     }
 
